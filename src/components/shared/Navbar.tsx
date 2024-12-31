832ce263e600502a4983eb898c90b4100bca7408
NoobMahbub <Mahbubbengali@gmail.com>
2024-12-31 20:20:20 +0600

    ✅ Feat: added hamburger

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -10,6 +10,7 @@ import {
 } from "../ui/navigation-menu";
 import { Switch } from "../ui/switch";
 import { Button } from "../ui/button";
+import { AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -25,15 +26,17 @@ const Navbar = () => {
         <NavigationMenu className="hidden lg:flex ">
           <NavigationMenuList>
             <NavigationMenuItem className="flex space-x-8 items-center">
-              <NavigationMenuLink href="/news" className={` hover:text-red-500`}>
+              <NavigationMenuLink
+                href="/news"
+                className={` hover:text-red-500`}
+              >
                 News
               </NavigationMenuLink>
 
-              <div
-               
-                className={`hover:text-red-500`}
-              >
-                <NavigationMenuTrigger className="dark:bg-gray-900 dark:text-white">Sevices</NavigationMenuTrigger>
+              <div className={`hover:text-red-500`}>
+                <NavigationMenuTrigger className="dark:bg-gray-900 dark:text-white">
+                  Sevices
+                </NavigationMenuTrigger>
                 <NavigationMenuContent>
                   <ul className="text-gray-600 shadow-md rounded-md px-5 py-4 space-y-2">
                     <li>
@@ -55,7 +58,10 @@ const Navbar = () => {
                 </NavigationMenuContent>
               </div>
 
-              <NavigationMenuLink href="/about" className={`hover:text-red-500`}>
+              <NavigationMenuLink
+                href="/about"
+                className={`hover:text-red-500`}
+              >
                 About
               </NavigationMenuLink>
 
@@ -70,13 +76,17 @@ const Navbar = () => {
         </NavigationMenu>
 
         {/* color switcher and login button */}
-        <div className="hidden lg:flex">
+        <div className="hidden lg:flex space-x-3">
           <div className="flex justify-center items-center space-x-3">
             <span>Dark Mode</span>
-            <Switch/>
+            <Switch />
             <Button variant="default">Login</Button>
-
           </div>
+        </div>
+        <div className="lg:hidden">
+          <Button variant={"default"}>
+            <AiOutlineMenu size={24} />
+          </Button>
         </div>
       </nav>
     </header>
