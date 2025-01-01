6ceee8b87fe0276429260e15605ee4ccf398aeca
NoobMahbub <Mahbubbengali@gmail.com>
2025-01-01 17:21:56 +0600

    ✅ Update:  replaced with arrow function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+const Home = async() => {
 
-export default function Home() {
+  
   return (
-    <div>
-      Home
+    <div className="py-12">
+    <div className="my-12">
+          <h2 className="text-2xl font-bold mb-8">Latest News</h2>
+        </div>
     </div>
   );
 }
+
+export default Home
