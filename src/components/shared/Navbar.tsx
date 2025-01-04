a5fc948ec284326108c49674955d8bddfca6d127
NoobMahbub <Mahbubbengali@gmail.com>
2025-01-04 23:53:45 +0600

    Added active nav link highlight using usePathname

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,18 +1,20 @@
+"use client"
 import Link from "next/link";
 import React from "react";
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "../ui/navigation-menu";
 import { Switch } from "../ui/switch";
 import { Button } from "../ui/button";
 import { AiOutlineMenu } from "react-icons/ai";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname()
   return (
     <header className="shadow-md py-4">
       <nav className="flex justify-between items-center max-w-7xl mx-auto px-4">
@@ -26,51 +28,51 @@ const Navbar = () => {
         <NavigationMenu className="hidden lg:flex ">
           <NavigationMenuList>
             <NavigationMenuItem className="flex space-x-8 items-center">
-              <NavigationMenuLink
+              <Link
                 href="/news"
-                className={` hover:text-red-500`}
+                className={pathname==='/news'? "text-red-500" : "hover:text-red-500"}
               >
                 News
-              </NavigationMenuLink>
+              </Link>
 
-              <div className={`hover:text-red-500`}>
+              <div className={pathname==='/services'? "text-red-500" : "hover:text-red-500"}>
                 <NavigationMenuTrigger className="dark:bg-gray-900 dark:text-white">
                   Sevices
                 </NavigationMenuTrigger>
                 <NavigationMenuContent>
                   <ul className="text-gray-600 shadow-md rounded-md px-5 py-4 space-y-2">
                     <li>
-                      <NavigationMenuLink href="/services/web">
+                      <Link href="/services/web">
                         Web Development
-                      </NavigationMenuLink>
+                      </Link>
                     </li>
                     <li>
-                      <NavigationMenuLink href="/services/app">
+                      <Link href="/services/app">
                         Mobile Apps
-                      </NavigationMenuLink>
+                      </Link>
                     </li>
                     <li>
-                      <NavigationMenuLink href="/services/seo">
+                      <Link href="/services/seo">
                         SEO
-                      </NavigationMenuLink>
+                      </Link>
                     </li>
                   </ul>
                 </NavigationMenuContent>
               </div>
 
-              <NavigationMenuLink
+              <Link
                 href="/about"
-                className={`hover:text-red-500`}
+                className={pathname==='/about'? "text-red-500" : "hover:text-red-500"}
               >
                 About
-              </NavigationMenuLink>
+              </Link>
 
-              <NavigationMenuLink
+              <Link
                 href="/contact"
-                className={`hover:text-red-500`}
+                className={pathname==='/contact'? "text-red-500" : "hover:text-red-500"}
               >
                 Contact
-              </NavigationMenuLink>
+              </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
