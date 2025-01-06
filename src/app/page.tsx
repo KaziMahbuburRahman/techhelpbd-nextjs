bf913f4553648e055a20698b5c42196d4dd4cc69
NoobMahbub <Mahbubbengali@gmail.com>
2025-01-06 20:21:18 +0600

    fetched data from api and passed to news component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Banner from "@/components/shared/Banner";
+import NewsCard from "@/components/shared/NewsCard";
+import { NewsItem } from "@/types/news";
 
 const Home = async () => {
+  const news = await fetch(
+    "https://news-api-next-js-kappa.vercel.app/api/news"
+  ).then((res) => res.json());
+  console.log(news);
+
   return (
     <div>
       <Banner />
+      <div>
+        <h2 className="text-3xl font-bold">Latest News</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 py-2">
+          {
+            news.map((item:NewsItem)=>(
+              <NewsCard key={item?._id} item={item} />
+            ))
+          }
+        </div>
+      </div>
     </div>
   );
 };
